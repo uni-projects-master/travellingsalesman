aaaaf251b0946e8206b35aaa29e2308804411019
Solmaz Mohammadi <solmaz454@gmail.com>
2022-04-26 17:44:03 +0200

    Data structure is almost complete
    long/lat conversion to radians
    euc distance
    geo distance

--- TSP.py
+++ TSP.py
@@ -1,6 +1,7 @@
 import random
 import os
 import gc
+import math
 from time import perf_counter_ns
 
 
@@ -19,10 +20,27 @@ class Graph:
 		
 		for vertex in verteces:
 			vertex = vertex.split()
+			if vertex and 'EOF' not in vertex:
+				self.vertex_list.append(Vertex(vertex[0], float(vertex[1]), float(vertex[2])))
+		#print("BEFORE CONVERSION")
+		#print(self.vertex_list[0].x, "  ", self.vertex_list[0].y)
+		if self.file_type == 'GEO':
+			self.geo_format()
+		#print("AFTER CONVERSION")
+		#print(self.vertex_list[0].x, "  ", self.vertex_list[0].y)
 
-			if vertex or vertex[0] != 'EOF':
-				print(vertex)
-				self.vertex_list.append(Vertex(vertex[0], vertex[1], vertex[2]))
+	def geo_format(self):
+		PI = 3.141592
+		# CONVERTING LATITUDE AND LONGTITUDE TO RADIAN
+		for i in self.vertex_list:
+			latitude = int(i.x)
+			min_x = i.x - latitude
+			radian_x = PI*(latitude + 5.0 * min_x / 3.0)/180.0
+			longitude = int(i.y)
+			min_y = i.x - longitude
+			radian_y = PI * (longitude + 5.0 * min_y / 3.0) / 180.0
+			i.x = radian_x
+			i.y = radian_y
 
 	def get_graph(self):
 		print('--------------------dimension----------------')
@@ -36,6 +54,22 @@ class Graph:
 			print(self.vertex_list[i].y)
 
 
+def geo_distance(point1, point2):
+	RRR = 6378.388
+	q1 = math.cos(point1.y - point2.y)
+	q2 = math.cos(point1.x - point2.x)
+	q3 = math.cos(point1.x + point2.x)
+	geo_d = int(RRR*math.acos(0.5*((1.0 + q1)*q2 - (1.0 - q1)*q3)))
+	return geo_d
+
+
+def euc_distance(point1, point2):
+	d1 = (point1.x - point2.x)**2
+	d2 = (point1.y - point2.y)**2
+	euc_d = round(math.sqrt(d1 + d2))
+	return euc_d
+
+
 if __name__ == '__main__':
 
 	dir_name = 'tsp_dataset'
@@ -45,7 +79,7 @@ if __name__ == '__main__':
 		filename = os.fsencode(file)
 		filename = filename.decode("utf-8")
 
-		if(filename.endswith('.tsp')):
+		if filename.endswith('.tsp'):
 			print('looking at: ', filename)
 			f = open(dir_name + '/' + filename)
 			f.readline()
@@ -60,5 +94,6 @@ if __name__ == '__main__':
 			f.readline()
 			verteces = f.read().splitlines()
 			g = Graph(dimension, file_type, verteces)
-			g.get_graph()
+
+
 
